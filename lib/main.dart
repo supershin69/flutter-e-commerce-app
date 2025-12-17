f3a37316afb62c30f6ea0789fe79601d6c919df5
shiro <mgshinthant58@gmail.com>
2025-12-17 14:35:15 +0630

    Added superbase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://mxngcloeolzkfnauioln.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14bmdjbG9lb2x6a2ZuYXVpb2xuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5NDk2NDcsImV4cCI6MjA4MTUyNTY0N30.-4mju9Eyce84EB_SodaOhAK9vIunJbuXuvuukXfr99g',
+  );
+
+  debugPrint('Supabase initialized: '
+    '${Supabase.instance.client.auth.currentSession}');
+
+
   runApp(const MyApp());
 }
 
