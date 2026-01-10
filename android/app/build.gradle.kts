6aea0e8e052c05a5123385f3b36cdc9a492399a7
shiro <mgshinthant58@gmail.com>
2026-01-10 20:35:20 +0630

    Configured Notification Channel

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin") // Flutter plugin
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -10,20 +11,33 @@ android {
 
     defaultConfig {
         applicationId = "com.example.e_commerce_frontend"
-        minSdk = flutter.minSdkVersion                      // minimum supported Android version
+        minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+
+        multiDexEnabled = true 
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        // Use Java 17 for Java code
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"  // 🔑 Must match Java version
+        // Use Java 17 for Kotlin code (Simple string assignment)
+        jvmTarget = "17"
     }
+}
+
+dependencies {
+    // Kotlin DSL syntax: use parentheses and double quotes
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
 
 flutter {
